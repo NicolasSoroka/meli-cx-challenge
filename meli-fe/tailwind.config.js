c407205fa2127d2425c566697264cda4bba7b2e7
Nicolas Soroka <nsoroka@quadiontech.com>
2024-05-02 00:00:49 -0300

    base template - colors

---
+++ meli-fe/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: 
+    [
+      './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+    ],
+  theme: {
+    colors: {
+      meli_yellow : '#FFE600',
+      meli_green : '#00A650',
+      meli_gray: '#EBEBEB',
+      meli_blue : '#3483FA'
+    },
+    extend: {}
+  },
+  plugins: []
+}
